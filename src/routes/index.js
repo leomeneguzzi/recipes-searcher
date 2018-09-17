f5fc96470481facf12e7162e481122718c98e137
Leonardo Meneguzzi <meneguzzileo@gmail.com>
2018-09-17 03:22:49 -0300

    Feature done, bad code, needing refactor and tests

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import helloResponse from './controllers/helloController';
+import recipesGet from './controllers/recipesController';
 
 const routes = (server) => {
   server.get('/hello/:name', helloResponse);
+  server.get('/recipes/', recipesGet);
 };
 
 export default routes;
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,19 @@
-import { createServer } from 'restify';
+import { createServer, plugins } from 'restify';
+import errs from 'restify-errors';
 import routes from '../routes';
 
 const server = createServer();
+server.use(plugins.queryParser());
 
-server.on('restifyError', (req, res, err, cb) => {
-  console.log('deu ruim!');
-  return cb();
+server.on('restifyError', (req, res, err) => {
+  console.log(err);
+  if (err.response) {
+    res.send(new errs.InternalServerError('Ocorreu um erro interno!'));
+  } else if (err.request) {
+    res.send(new errs.ServiceUnavailableError('Serviço indisponível!'));
+  } else {
+    res.send(new errs.NotImplementedError('Algo deu errado!'));
+  }
 });
 
 routes(server);
