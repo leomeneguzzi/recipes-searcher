265492d01cf73482a3e3b48308511558a7053ab8
Leonardo Meneguzzi <meneguzzileo@gmail.com>
2018-09-19 17:48:35 -0300

    Added argument validation

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -1,7 +1,17 @@
+import errs from 'restify-errors';
 import mountRecipesJson from '../services/mountRecipeService';
 
 async function response(req, res, next) {
   const ingredients = req.query.i.split(',');
+
+  if (req.query.i.split(',').length > 3) {
+    res.send(
+      new errs.InvalidArgumentError(
+        'Quantidade máxima de ingredientes permitido é 3!',
+      ),
+    );
+    return next();
+  }
 
   try {
     const mountedResponseJson = await mountRecipesJson(
